ad79253fbadb76a3601cbbf94ad7b1e4ceac7435
caozxin1230 <33402671+caozxin1230@users.noreply.github.com>
2018-12-17 12:20:50 -0500

    Create otc_data.py

---
+++ otc_data.py
@@ -0,0 +1,8 @@
+class otc_data:
+
+    def __init__(self, listing_date, symbol,effective_date, cash_amount ):
+        self.listing_date = listing_date
+
+        self.symbol = symbol
+        self.effective_date = effective_date
+        self.cash_amount = cash_amount
